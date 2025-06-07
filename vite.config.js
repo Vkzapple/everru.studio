0d51fde7464996741145515f4faa0db564a24d85
Vkzapple <evellykhnz@gmail.com>
2025-06-07 21:41:19 +0700

    others

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ["fontawesome"],
+          vendor: ["@fortawesome/fontawesome-svg-core"],
         },
       },
     },
