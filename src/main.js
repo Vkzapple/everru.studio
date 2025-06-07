aebd2c2dd1cbb7c8e169830d22194b348d48912a
Vkzapple <evellykhnz@gmail.com>
2025-06-07 21:26:57 +0700

    first

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import logo from "./assets/logo.jpg";
-import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
   <div>
